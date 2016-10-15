4d18ac4cab95767e2d37774545d86010df725056
Karl Lam <karl.v.lam@gmail.com>
2016-10-15 11:25:52 +0800

    ok

--- test/test-node.js
+++ test/test-node.js
@@ -6,10 +6,11 @@ eval( fs.readFileSync(__dirname+'/../sebrina.js', 'utf8'))
 
 
 var wordsToSebrina = [
-    'Dear 老婆仔',
-    'I love you!'
+    '纖雲弄巧，飛星傳恨，銀漢迢迢暗度。金風玉露一相逢，便勝卻人間無數。',
+    '柔情似水，佳期如夢，忍顧鵲橋歸路！兩情若是久長時，又豈在朝朝暮暮！'
 ]
 
+console.log('Start testing..', JSON.stringify(wordsToSebrina,null,2))
 wordsToSebrina.forEach(function(words){
     sebrina(ss(words), function(err, res){
         assert.deepEqual(words, res) 
@@ -18,5 +19,5 @@ wordsToSebrina.forEach(function(words){
         assert.deepEqual(words, res) 
     })
 })
-
+console.log('Test success!')
 
